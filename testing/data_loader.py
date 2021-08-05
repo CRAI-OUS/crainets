cf10c249555603206841cd2cb327569bee82e628
JonNesvold <jon.nesvold@pm.me>
2021-08-05 09:01:35 +0000

    reducing action resources

--- testing/data_loader.py
+++ testing/data_loader.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision
 import testing.config as config
+import torch.utils.data as data_utils
 
 transform = torchvision.transforms.Compose(
     [torchvision.transforms.ToTensor(),
@@ -21,17 +22,17 @@ test = torchvision.datasets.CIFAR10(
                     config.TESTING, train=False, download=True,
                     transform=transform_test)
 
-# idx = torch.arange(10000)
-# trainx = data_utils.Subset(train, idx)
-# testx = data_utils.Subset(test, idx)
+idx = torch.arange(1000)
+trainx = data_utils.Subset(train, idx)
+testx = data_utils.Subset(test, idx)
 
 train_loader = torch.utils.data.DataLoader(
-                        train,
+                        trainx,
                         batch_size=config.batch_size_train,
                         shuffle=True
                     )
 test_loader = torch.utils.data.DataLoader(
-                        test,
+                        testx,
                         batch_size=config.batch_size_test,
                         shuffle=True
                     )
